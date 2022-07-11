b54450167c7e15ec2d5a125a625568c6618d05c2
Michael Okello <okellofrost@gmail.com>
2022-07-11 17:11:47 +0300

    Yelp Array

---
+++ index.js
@@ -0,0 +1,9 @@
+let priceRanges = [
+    {label : '$', toolTip : 'Inexpensive', minPerPerson : 0 , MaxPerPerson:10},
+    {label : '$$', toolTip : 'Moderteve', minPerPerson : 10 , MaxPerPerson:20},
+    {label : '$$$', toolTip : 'Expensive', minPerPerson : 21 , MaxPerPerson:50}
+]
+
+let restaurants = [
+    {averagePerPerson : 5}
+]
